8ee144569c95bc6671074e6b2fbcec75c9091ea1
AndreL1 <andre.leme@verifone.com>
2019-10-09 11:30:29 -0300

    Listar colunas da tabela

--- Listar colunas da tabela.sql
+++ Listar colunas da tabela.sql
@@ -1,6 +1,17 @@
-s---------------------------------------------------------------------------------
--- Listar colunas da tabela (Views de sistema)
+---------------------------------------------------------------------------------
+--                         Listar colunas da tabela
+---------------------------------------------------------------------------------
+
+------------------------------------------
+-- Views de sistema
+------------------------------------------
 
 select * from information_schema.columns where table_name = 'nomeTabela'
 select * from sys.all_columns where object_id = object_id('nomeTabela')
 select * from sys.columns where object_id = object_id('nomeTabela')
+
+------------------------------------------
+-- Stored Procedures
+------------------------------------------
+execute sp_columns nomeTabela
+execute sp_help nomeTabela
